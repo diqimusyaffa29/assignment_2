760e3b44ea70baedfb8e653805e28ba4f4507289
Diqi Laptop <diqimusyaffa4@gmail.com>
2023-10-19 10:58:22 +0800

    First Commit

---
+++ main.js
@@ -0,0 +1,114 @@
+
+// Validate Form inputs before submit data
+function validateForm() {
+    let name = document.getElementById("name").value;
+    let age = document.getElementById("age").value;
+    let address = document.getElementById("address").value;
+    let email = document.getElementById("email").value;
+
+    if (name == "") {
+        alert("Name is required");
+        return false;
+    }
+
+    if (age == "") {
+        alert("Age is required");
+        return false;
+    } else if (age < 1) {
+        alert("Age must not be 0 or less than Zero")
+        return false;
+    }
+
+    if (address == "") {
+        alert("Address is required");
+        return false;
+    }
+
+    if (email == "") {
+        alert("Email is required");
+        return false;
+    } else if (!email.includes("@")) {
+        alert("Invalid Email Address")
+        return false;
+    }
+
+    return true;
+}
+
+
+// function to show data from local storage
+function showData() {
+    let peopleList;
+    if (localStorage.getItem("peopleList") == null) {
+        peopleList = [];
+    } else {
+        peopleList = JSON.parse(localStorage.getItem("peopleList"));
+    }
+
+    let html = "";
+
+    peopleList.forEach(function (element, index) {
+        html += "<tr>";
+        html += "<td>" + element.name + "</td>";
+        html += "<td>" + element.age + "</td>";
+        html += "<td>" + element.address + "</td>";
+        html += "<td>" + element.email + "</td>";
+        html +=
+            '<td><button onclick="deleteData(' + index + ')" class="btn btn-danger">Delete</button> <button onclick="updateData(' + index + ')" class="btn btn-warning m-2">Edit</button></td>';
+        html += "</tr>";
+    });
+
+    document.querySelector("#crudTable tbody").innerHTML = html;
+
+}
+
+// Loads All data from local storage when document or page loaded
+document.onload = showData();
+
+
+// function to add data to local storage
+function addData() {
+    // if form is validate
+    if (validateForm() == true) {
+        let name = document.getElementById("name").value;
+        let age = document.getElementById("age").value;
+        let address = document.getElementById("address").value;
+        let email = document.getElementById("email").value;
+
+        let peopleList;
+        if (localStorage.getItem("peopleList") == null) {
+            peopleList = [];
+        } else {
+            peopleList = JSON.parse(localStorage.getItem("peopleLlist"));
+        }
+
+        peopleList.push({
+            name: name,
+            age: age,
+            address: address,
+            email: email,
+        });
+
+        localStorage.setItem("peopleList", JSON.stringify(peopleList));
+        showData();
+        document.getElementById("name").value = "";
+        document.getElementById("age").value = "";
+        document.getElementById("address").value = "";
+        document.getElementById("email").value = "";
+    }
+}
+
+
+// function to delete Data from local storage
+function deleteData(index) {
+    let peopleList;
+    if (localStorage.getItem("peopleList") == null) {
+        peopleList = [];
+    } else {
+        peopleList = JSON.parse(localStorage.getItem("peopleLlist"));
+    }
+
+    peopleList.slice(index, 1);
+    localStorage.setItem("peopleList", JSON.stringify(peopleList));
+    showData();
+}
